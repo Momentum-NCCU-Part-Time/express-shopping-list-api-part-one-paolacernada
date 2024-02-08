a882cc4ad3e1f5b0baec801c7eb7de029d521f4d
Paola Cernada <96987381+paolacernada@users.noreply.github.com>
2024-02-08 18:29:02 -0500

    added express middleware

--- server.js
+++ server.js
@@ -1,16 +1,17 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
 require('dotenv').config();
 
 // connect to MongoDB
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGODB_URI)
   .then(() => console.log('Connected to MongoDB...'))
   .catch(err => console.error('Could not connect to MongoDB...', err));
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
 
 const ShoppingList = require('./models/ShoppingList');
 
