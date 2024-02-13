e2b030cd022888f5ac850b44c1a5aa6faed61bf5
Paola Cernada <96987381+paolacernada@users.noreply.github.com>
2024-02-13 17:14:33 -0500

    Updated schema

--- models/ShoppingList.js
+++ models/ShoppingList.js
@@ -1,11 +1,31 @@
 const mongoose = require('mongoose');
 
+const ItemSchema = new mongoose.Schema({
+  name: { 
+    type: String, 
+    required: true },
+  quantity: { 
+    type: Number, 
+    required: true, 
+    min: 1 },
+  done: { 
+    type: Boolean, 
+    default: false },
+  createdAt: { 
+    type: Date, 
+    default: Date.now },
+  updatedAt: { 
+    type: Date, 
+    default: Date.now }
+});
+
 const ShoppingListSchema = new mongoose.Schema({
   name: {
     type: String,
     required: true,
     maxlength: 50,
   },
+  items: [ItemSchema],
   createdAt: {
     type: Date,
     default: Date.now,
